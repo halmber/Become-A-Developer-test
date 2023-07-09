dd7ef78616a3723274c2b903fee89730d9e32ede
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-07-09 15:57:59 +0300

    version 1.0. using object for counter

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+type TUnique = Object & {
+    [key: string]: number;
+};
+
+const findUniqueFromEntity = (arr: string[] | string): string => {
+    const unique: TUnique = {};
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+
+        if (unique[item]) {
+            unique[item]++;
+        } else {
+            unique[item] = 1;
+        }
+    }
+    for (const key in unique) {
+        if (unique[key] === 1) {
+            return key;
+        }
+    }
+    return "there is not what is needed";
+};
+
+const findUniqueLetter = (str: string) => {
+    const words = str.split(/[.,:;\s-!?'"()]+/);
+    const uniqueInWords = words.map((item) => findUniqueFromEntity(item));
+
+    return findUniqueFromEntity(uniqueInWords);
+};
+
+const input = `The Tao gave birth to machine language. Machine language gave birth to the assembler. The assembler gave birth to the compiler.  Now there are ten thousand languages. Each language has its purpose, however humble. Each language expresses the Yin and Yang of software. Each language has its place within the Tao. But do not program in COBOL if you can avoid it. -- Geoffrey James, "The Tao of Programming"`;
+const input2 = `C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (c) Bjarne Stroustrup`;
+console.log(findUniqueLetter(input));
